3c108b45f558e07ae15549283e8452bf84dcc5c2
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-22 22:30:52 -0400

    added file

--- main.cpp
+++ main.cpp
@@ -5,3 +5,30 @@ Instructor: Genady Maryash
 Assignment: E6.1
 The program creates an array and gives some output. 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+bool equals(int a[], int a_size, int b[], int b_size) {
+	if(a_size == b_size) {
+		for (int i = 0; i < a_size; i++) {
+			if(a[i] != b[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	else{return false;}
+}
+
+int main() {
+	int a[] = {0, 1, 2, 3, 4, 5};
+	int b[] = {0, 1, 2, 3, 4, 5}; 
+	
+	cout << equals(a, 6, b, 6) << endl;
+	
+	return 0;
+}
